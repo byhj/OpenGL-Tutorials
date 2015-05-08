2c8ff354802489eb2cca6634d97a8c1eb5cb34eb
binyu <binyuapple@163.com>
2015-05-08 19:47:50 +0800

    Create

---
+++ ch02-Triangle/ch02-Triangle.cpp
@@ -0,0 +1,114 @@
+#include <GL/glew.h>
+#include <GL/glfw3.h>
+#include <iostream>
+
+#include "shader.h"
+
+const GLuint Width(1200), Height(1024);     //window size
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode);
+void init();
+void render();
+
+int main()
+{
+	std::cout << "Starting GLFW context, OpenGL3.3" << std::endl;
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); //opengl 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //using opengl core file
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	// Create a GLFWwindow object that we can use for GLFW's functions
+	GLFWwindow *window = glfwCreateWindow(Width, Height, "OpenGL Tutorial", nullptr, nullptr);
+	glfwMakeContextCurrent(window);
+	if (window == NULL)
+	{
+		std::cerr << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwSetKeyCallback(window, key_callback);  //keyboard function
+	glewExperimental = GL_TRUE;
+
+	// Initialize GLEW to setup the OpenGL Function pointers
+	if (glewInit() != GLEW_OK)
+	{
+		std::cerr << "Failed to initializz GLEW" << std::endl;
+		return -1;
+	}
+	init();
+
+	glViewport(0, 0, Width, Height);
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		render();
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+Shader TriangleShader("Triangle Shader");
+GLuint vao, vbo;
+GLuint program;
+
+static const GLfloat VertexData[] = 
+{
+	-1.0f, -1.0f, 0.0f,
+	 1.0f, -1.0f, 0.0f,
+	 0.0f,  1.0f, 0.0f
+};
+void init_buffer()
+{
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(VertexData), VertexData, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+void init_shader()
+{
+	TriangleShader.init();
+	TriangleShader.attach(GL_VERTEX_SHADER, "triangle.vert");
+	TriangleShader.attach(GL_FRAGMENT_SHADER, "triangle.frag");
+	TriangleShader.link();
+	program = TriangleShader.program;
+}
+
+void init_vertexArray()
+{
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glBindVertexArray(0);
+}
+
+void init()
+{
+	init_shader();
+	init_buffer();
+	init_vertexArray();
+}
+
+
+
+void render()
+{
+	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glUseProgram(program);
+	glBindVertexArray(vao);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glBindVertexArray(0);
+	glUseProgram(0);
+}
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);  //we should close the window
+}
---
+++ ch03-Matrix/ch03-Matrix.cpp
@@ -0,0 +1,130 @@
+#include <GL/glew.h>
+#include <GL/glfw3.h>
+#include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "shader.h"
+
+const GLuint Width(1200), Height(800);     //window size
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode);
+void init();
+void render();
+
+int main()
+{
+	std::cout << "Starting GLFW context, OpenGL3.3" << std::endl;
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); //opengl 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //using opengl core file
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	// Create a GLFWwindow object that we can use for GLFW's functions
+	GLFWwindow *window = glfwCreateWindow(Width, Height, "OpenGL Tutorial", nullptr, nullptr);
+	glfwSetWindowPos(window, 300, 50);
+	glfwMakeContextCurrent(window);
+	if (window == NULL)
+	{
+		std::cerr << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwSetKeyCallback(window, key_callback);  //keyboard function
+	glewExperimental = GL_TRUE;
+
+	// Initialize GLEW to setup the OpenGL Function pointers
+	if (glewInit() != GLEW_OK)
+	{
+		std::cerr << "Failed to initializz GLEW" << std::endl;
+		return -1;
+	}
+	init();
+
+	glViewport(0, 0, Width, Height);
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		render();
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+Shader TriangleShader("Triangle Shader");
+GLuint vao, vbo;
+GLuint program;
+GLuint mvp_loc;
+
+static const GLfloat VertexData[] = 
+{
+	-1.0f, -1.0f, 0.0f,
+	 1.0f, -1.0f, 0.0f,
+	 0.0f,  1.0f, 0.0f
+};
+void init_buffer()
+{
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(VertexData), VertexData, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+void init_shader()
+{
+	TriangleShader.init();
+	TriangleShader.attach(GL_VERTEX_SHADER, "triangle.vert");
+	TriangleShader.attach(GL_FRAGMENT_SHADER, "triangle.frag");
+	TriangleShader.link();
+	program = TriangleShader.program;
+
+	mvp_loc = glGetUniformLocation(program, "mvp");
+
+}
+
+void init_vertexArray()
+{
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glBindVertexArray(0);
+}
+
+void init()
+{
+	init_shader();
+	init_buffer();
+	init_vertexArray();
+}
+
+
+
+void render()
+{
+	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glm::mat4 model = glm::mat4(1.0f);
+	glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f), glm::vec3(0.0f, 0.0f, 0.0f),
+		                         glm::vec3(0.0f, 1.0f, 0.0f) );
+	glm::mat4 proj = glm::perspective(45.0f, float(Width) / Height, 0.1f, 1000.0f);
+	glm::mat4 mvp = proj * view * model;
+
+	glUseProgram(program);
+	glUniformMatrix4fv(mvp_loc, 1, GL_FALSE, &mvp[0][0]);
+	glBindVertexArray(vao);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glBindVertexArray(0);
+	glUseProgram(0);
+}
+
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);  //we should close the window
+}
